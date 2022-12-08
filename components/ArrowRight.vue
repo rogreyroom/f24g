7f3f3367fa50673447efa0c4a3ecd1b7c89011d0
Robert <robert@adamczewski.me>
2022-12-08 19:25:30 +0100

    feat: create arrow components

---
+++ components/ArrowRight.vue
@@ -0,0 +1,16 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" width="10.547" height="19.094" viewBox="0 0 10.547 19.094">
+    <g id="Group_4790" data-name="Group 4790" transform="translate(1639.92 184.048) rotate(180)">
+      <path id="Path_70" data-name="Path 70" d="M3695.661,165.661l8.84,8.84-8.84,8.84" transform="translate(-2065.581)" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="2"/>
+    </g>
+  </svg>
+</template>
+
+<style lang="scss" scoped>
+  svg {
+    transform: rotate(-180deg);
+    path {
+      stroke: greenyellow;
+    }
+  }
+</style>
